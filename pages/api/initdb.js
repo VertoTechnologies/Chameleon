6a14f3d52446b126d717179cb4dd66dc534e8436
Ahmad Hassan <ahmad.liberty9999@GMAIL.COM>
2024-07-09 12:26:03 +0500

    create a function to create unique ID

--- pages/api/initdb.js
+++ pages/api/initdb.js
@@ -2,14 +2,25 @@ import dbConnect from '../../app/middleware/mongodb';
 import User from '../../app/models/user';
 import { interests } from '../../app/models/enums';
 
+
+function generateUniqueKey() {
+  let uniqueKey = Date.now().toString(36); // Base 36 timestamp
+  for (let i = 0; i < 8; i++) { // Generate 8 random characters
+    uniqueKey += Math.random().toString(36).charAt(2); // Append a random alphanumeric character
+  }
+  return uniqueKey;
+}
+
 export default async function handler(req, res) {
   console.log('API Route Hit'); // Debugging line to ensure the route is hit
   await dbConnect();
 
+  const userID = generateUniqueKey();
+
   const users = [{
-    userId: "user1",
+    userId: userID,
     name: "John Doe",
-    email: "john.doe@example.com",
+    email: "john.doe4@example.com",
     password: "password123",
     dateOfBirth: "1990-01-01",
     userDescription: "Avid music enthusiast and coffee lover.",
