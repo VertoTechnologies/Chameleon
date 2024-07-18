fdec4f7c367181ef41facc03f9527bdcfd05e308
Ahmad Hassan <ahmad.liberty9999@GMAIL.COM>
2024-07-18 16:13:33 +0500

    Update viewProfile.js

--- src/pages/api/viewProfile.js
+++ src/pages/api/viewProfile.js
@@ -3,7 +3,6 @@ import User from '../../models/user';
 // Optional: Import authentication middleware
 
 export default async function userProfile(req, res) {
-  console.log(req.method)
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
@@ -12,15 +11,18 @@ export default async function userProfile(req, res) {
 
   // Assuming the user ID is passed as a query parameter
   const { userId } = req.query;
+  if (!userId) {
+    return res.status(400).json({ message: 'User ID is required' });
+  }
 
   try {
     await dbConnect();
 
-    const user = await User.findOne( { userId: userId }).select('-password'); // Exclude the password field
+    const user = await User.findOne({userId:userId}).select('-password'); // Exclude the password field
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
-    console.log(json(user))
+    // console.log(json(user))
     res.status(200).json(user); // Send the user profile information
   } catch (error) {
     console.error("Error fetching user profile", error);
