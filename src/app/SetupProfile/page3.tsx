fdebcabc686d14d9a4788ae753bee38651e8b6e3
Fatima <126266646+fatima-kz@users.noreply.github.com>
2024-08-02 12:27:56 +0500

    Changes profile creation view

--- src/app/SetupProfile/page3.tsx
+++ src/app/SetupProfile/page3.tsx
@@ -1,13 +1,16 @@
+'use client'
+
 import React from 'react';
-import PersonalizationForm from './PersonalizationForm'
-import Border from '../components/authComponents/FormBorder';
+import Setup2 from './SetupProfile';
 import Background from '../components/authComponents/Background';
+import Border from '../components/authComponents/FormBorder';
+
 
 const Page: React.FC = () => {
   return (
     <div className="sign-up-layout rounded-4xl relative overflow-hidden">
       <div className="form-container flex rounded-4xl h-auto relative z-10">
-        <PersonalizationForm />
+        <Setup2 />
         <Border />
       </div>
       <Background />
